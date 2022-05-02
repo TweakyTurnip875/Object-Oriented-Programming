e36ba0729b4cf00efcc97f4a03afa102fcb460ec
TweakyTurnip875 <44481502+TweakyTurnip875@users.noreply.github.com>
2022-05-02 13:34:30 -0600

    Exercise 14 skeleton

---
+++ Chapter 17/Exercise14/src/com/noah/Main/Main.java
@@ -0,0 +1,17 @@
+package com.noah.Main;
+
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+	}
+	public static void prompt() {
+		
+	}
+	public static void encryptData() {
+		
+	}
+}
