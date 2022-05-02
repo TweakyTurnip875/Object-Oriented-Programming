078627ad793f2af19791340eb3bc241b4cb0a39b
TweakyTurnip875 <44481502+TweakyTurnip875@users.noreply.github.com>
2022-05-02 13:53:10 -0600

    prompt outline

--- Chapter 17/Exercise14/src/com/noah/Main/Main.java
+++ Chapter 17/Exercise14/src/com/noah/Main/Main.java
@@ -1,16 +1,34 @@
 package com.noah.Main;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
+		prompt();
 	}
 	public static void prompt() {
+		Scanner userInputStream = new Scanner(System.in);
+		
+		System.out.print("Enter input file name: ");
+		String inFileName = userInputStream.next();
+		
+		System.out.print("Enter output file name: ");
+		String outFileName = userInputStream.next();
+		
+		File inputFile = new File(inFileName);
+		File outputFile = new File(outFileName);
 		
 	}
+
 	public static void encryptData() {
 		
 	}
