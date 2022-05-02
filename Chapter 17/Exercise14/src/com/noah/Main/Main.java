5c15160c5d4f27518cceebc44241a6329d44ee48
TweakyTurnip875 <44481502+TweakyTurnip875@users.noreply.github.com>
2022-05-02 14:08:00 -0600

    Revert "createFile function created, input file reader and output file reader try catch"
    This reverts commit e5d8664a0b6c0409022669204d5d5b633d9a85d4.

--- Chapter 17/Exercise14/src/com/noah/Main/Main.java
+++ Chapter 17/Exercise14/src/com/noah/Main/Main.java
@@ -12,41 +12,23 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws FileNotFoundException {
 		prompt();
 	}
-	public static void prompt() throws IOException {
+	public static void prompt() {
 		Scanner userInputStream = new Scanner(System.in);
 		
 		System.out.print("Enter input file name: ");
 		String inFileName = userInputStream.next();
 		
-		createFile(inFileName);
-		
 		System.out.print("Enter output file name: ");
 		String outFileName = userInputStream.next();
 		
-		try (
-			DataInputStream in = new DataInputStream(new FileInputStream(inFileName));
-			DataOutputStream out = new DataOutputStream(new FileOutputStream(outFileName));
-		) {
-			
-		}
+		File inputFile = new File(inFileName);
+		File outputFile = new File(outFileName);
+		
 	}
-	public static void createFile(String name) throws FileNotFoundException {
-		try (
-			DataOutputStream out = new DataOutputStream(
-			new FileOutputStream(name + ".dat"));
-		) {
-			out.writeInt(32);
-			out.writeInt(56);
-			out.writeInt(12);
-			
-			System.out.println("file created");
-		} catch(IOException ex) {
-			System.out.println(ex);
-		}
-	}
+
 	public static void encryptData() {
 		
 	}
