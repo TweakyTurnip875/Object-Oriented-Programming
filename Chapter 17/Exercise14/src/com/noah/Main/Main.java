e5d8664a0b6c0409022669204d5d5b633d9a85d4
TweakyTurnip875 <44481502+TweakyTurnip875@users.noreply.github.com>
2022-05-02 14:05:26 -0600

    createFile function created, input file reader and output file reader try catch

--- Chapter 17/Exercise14/src/com/noah/Main/Main.java
+++ Chapter 17/Exercise14/src/com/noah/Main/Main.java
@@ -12,23 +12,41 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		prompt();
 	}
-	public static void prompt() {
+	public static void prompt() throws IOException {
 		Scanner userInputStream = new Scanner(System.in);
 		
 		System.out.print("Enter input file name: ");
 		String inFileName = userInputStream.next();
 		
+		createFile(inFileName);
+		
 		System.out.print("Enter output file name: ");
 		String outFileName = userInputStream.next();
 		
-		File inputFile = new File(inFileName);
-		File outputFile = new File(outFileName);
-		
+		try (
+			DataInputStream in = new DataInputStream(new FileInputStream(inFileName));
+			DataOutputStream out = new DataOutputStream(new FileOutputStream(outFileName));
+		) {
+			
+		}
 	}
-
+	public static void createFile(String name) throws FileNotFoundException {
+		try (
+			DataOutputStream out = new DataOutputStream(
+			new FileOutputStream(name + ".dat"));
+		) {
+			out.writeInt(32);
+			out.writeInt(56);
+			out.writeInt(12);
+			
+			System.out.println("file created");
+		} catch(IOException ex) {
+			System.out.println(ex);
+		}
+	}
 	public static void encryptData() {
 		
 	}
