5a5c37929646ba900d5ef62d801035510a39f726
404kevnotfound <44481502+TweakyTurnip875@users.noreply.github.com>
2022-05-04 13:35:27 -0600

    Program skeleton created

---
+++ Chapter 17/Exercise15/src/com/noah/Main/Main.java
@@ -0,0 +1,18 @@
+package com.noah.Main;
+
+import java.io.File;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public static void decrypt(File inFile, File outFile) {
+		
+	}
+	
+	public static void decryptPrompt() {
+		
+	}
+}
