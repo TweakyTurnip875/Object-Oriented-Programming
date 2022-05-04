5954b0e7d20fa19d8c4802328044b89b946d4684
404kevnotfound <44481502+TweakyTurnip875@users.noreply.github.com>
2022-05-04 13:55:17 -0600

    decrypt() method outline created

--- Chapter 17/Exercise15/src/com/noah/Main/Main.java
+++ Chapter 17/Exercise15/src/com/noah/Main/Main.java
@@ -1,6 +1,13 @@
 package com.noah.Main;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -9,8 +16,15 @@ public class Main {
 		decryptPrompt();
 	}
 
-	public static void decrypt(File inFile, File outFile) {
-		
+	public static void decrypt(File inFile, File outFile) throws FileNotFoundException, IOException {
+		try (
+			DataInputStream in = new DataInputStream(new FileInputStream(inFile));
+			DataOutputStream out = new DataOutputStream(new FileOutputStream(outFile));
+		) {
+			
+		} catch(EOFException ex) {
+			
+		}
 	}
 	
 	public static void decryptPrompt() {
