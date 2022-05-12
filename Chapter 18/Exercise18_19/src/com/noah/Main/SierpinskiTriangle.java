142331555f7d36df73bd8489cd7dfd3bbc4daa46
404kevnotfound <44481502+TweakyTurnip875@users.noreply.github.com>
2022-05-12 14:19:16 -0600

    removed unused imports

--- Chapter 18/Exercise18_19/src/com/noah/Main/SierpinskiTriangle.java
+++ Chapter 18/Exercise18_19/src/com/noah/Main/SierpinskiTriangle.java
@@ -1,17 +1,11 @@
 package com.noah.Main;
 
 import javafx.application.Application;
-import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
 public class SierpinskiTriangle extends Application {
