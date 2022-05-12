26fce53da1713c13aef6a6dbb6fc8082ba760881
404kevnotfound <44481502+TweakyTurnip875@users.noreply.github.com>
2022-05-12 12:47:21 -0600

    implemented reverseDisplay functionality with recursion

--- Chapter 18/Exercise18_09/src/com/noah/Main/Main.java
+++ Chapter 18/Exercise18_09/src/com/noah/Main/Main.java
@@ -3,11 +3,16 @@ package com.noah.Main;
 public class Main {
 	
 	public static void main(String[] args) {
-
+		reverseDisplay("hello world");
 	}
 	
 	public static void reverseDisplay(String s) {
 		
+		System.out.print(s.charAt(s.length() - 1));
+		
+		if(!(s.length() <= 1)) {
+			reverseDisplay(s.substring(0, s.length() - 1));
+		}
 	}
 	
 }
